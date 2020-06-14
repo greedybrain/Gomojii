03ac2bd7cb9bc805ed385727e9d98e0b044aa756
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-14 01:14:42 -0400

    changed form class comp to a functional component

--- src/components/EmojiSearchForm.jsx
+++ src/components/EmojiSearchForm.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const EmojiSearchForm = () => {
-     return ( 
-          <div></div>
-      );
+const EmojiSearchForm = ({ handleEmojiSearch }) => {
+
+     const [query, setQuery] = useState('')
+
+     const handleOnChange = event => {
+          setQuery(event.target.value)
+     }
+
+     const handleOnSubmit = event => {
+          event.preventDefault()
+          
+          handleEmojiSearch(query)
+          
+          setQuery('')
+
+          event.target.reset()
+     }
+
+     return (
+          <div>
+               <form onSubmit={handleOnSubmit}>
+                    <input onChange={handleOnChange} type="search" />
+                    <button type="submit">Search</button>
+               </form>
+          </div>
+     );
 }
- 
+
 export default EmojiSearchForm;
