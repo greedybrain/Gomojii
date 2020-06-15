f5aae1700d6c1f685384ef6bf4bdf3f5e65c07d0
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-14 21:42:39 -0400

    created functionality to render all emoji cats

--- src/containers/SidebarNav.jsx
+++ src/containers/SidebarNav.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Logo from '../components/Logo';
 
-const SidebarNav = () => {
+const SidebarNav = ({ emojis, handleCategoryClick }) => {
+     // Inside of the sidenav we will have links/buttons displaying the names of each category/group
+
+     // 1- Create onclick event for each link.
+     // 2- Depending on the category that was clicked we will show all emojis relating to that category by changing the emojis array state in the EmojiContainer component
+
+     const renderSubCategories = emojis.map((emoji, index) => {
+          return <li onClick={handleCategoryClick} className="category" key={index}>{emoji.group}</li>
+     })
 
      return ( 
-          <>
-               Sidebar
+          <>   
+               <Logo />
+               <ul>
+                    { renderSubCategories }
+               </ul>
           </>
      );
 }
