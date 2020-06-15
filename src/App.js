2e8de19e5e78ecaa3899e6083932585a251ecaac
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-14 21:41:45 -0400

    removed sidebarnav component from app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import EmojiContainer from './containers/EmojiContainer';
-import SidebarNav from './containers/SidebarNav'
 
 function App() {
   return (
     <div className="App">
-      <SidebarNav />
       <EmojiContainer />
     </div>
   );
