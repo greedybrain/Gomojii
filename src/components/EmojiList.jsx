f84e7e79123e101847fc5985ed88350338c8160d
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-22 09:53:39 -0400

    fixed emoji filter and search functionality

--- src/components/EmojiList.jsx
+++ src/components/EmojiList.jsx
@@ -1,47 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const EmojiList = ({ emojis, emojisFiltered, emojiSearchResults }) => {
-     // took me forever to come up with this technique
-     const accordingEmojis = () => {
-          if (emojiSearchResults.length > 0) {
+class EmojiList extends Component {
+     
+     getEmojis() {
+          let { emojis, emojisFilteredState, emojiSearchResults } = this.props
+          if (emojiSearchResults !== null && emojiSearchResults.length > 0) {
                emojis = emojiSearchResults
-          } else if (emojisFiltered.length > 0) {
-               emojis = emojisFiltered
+          } else if (emojisFilteredState !== null && emojisFilteredState.length > 0) {
+               emojis = emojisFilteredState
+          } else {
+               return emojis
           }
           return emojis
      }
 
-     const currEmojis = accordingEmojis()
-
-     const renderEmojisOrNotFoundMessage =
-          // if emojis is truthy
-          currEmojis ? // then ...
-               currEmojis
-                    .map((emoji, index) => {
-                         return (
-                              <li className="emoji animate__animated animate__fadeIn animate__fast" key={index} title={emoji.unicodeName}>{emoji.character}</li>
-                         )
-                    })
-               : //otherwise
-               <li className="not-found-msg">
-                    <div className="message">
-                         No related emojis found
-                    </div>
-                    <div className="emoji-msg">
-                         <h1>
-                              <span role="img" aria-label="wonder emoji">🤔</span>
-                         </h1>
-                    </div>
-               </li>
-
-     return ( 
-          <ul>
-               <li className="results">
-                    Results: {currEmojis.length}
-               </li>
-               { renderEmojisOrNotFoundMessage }
-          </ul>
-     );
+     render() {
+          const currEmojis = this.getEmojis().length > 0 ? this.getEmojis() : []
+          const renderEmojis =
+               // if emojis is truthy
+               currEmojis ? // then ...
+                    currEmojis
+                         .map((emoji, index) => {
+                              return (
+                                   <li className="emoji animate__animated animate__fadeIn animate__fast" key={ index } title={ emoji.unicodeName }>{ emoji.character }</li>
+                              )
+                         })
+                    : //otherwise
+                    null
+                    // <li className="not-found-msg">
+                    //      <div className="message">
+                    //           No related emojis found
+                    //      </div>
+                    //      <div className="emoji-msg">
+                    //           <h1>
+                    //                <span role="img" aria-label="wonder emoji">🤔</span>
+                    //           </h1>
+                    //      </div>
+                    // </li>
+          return ( 
+               <ul>
+                    <li className="results">
+                         Results: { currEmojis !== null ? currEmojis.length : 0 }
+                    </li>
+                    { renderEmojis }
+               </ul>
+          );
+     }
 }
 
 export default EmojiList;
