aaf5d24026e6cc74e82b4ddf6f0a2e270d915c37
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-22 09:53:14 -0400

    begin reducer creation

---
+++ src/reducers/manageEmojis.js
@@ -0,0 +1,14 @@
+// const initialState = {
+//      emojis: [],
+//      emojisCategories: [],
+//      emojisFilteredState: [],
+//      emojiSearchResults: []
+// }
+
+// export default function manageEmojis(state = initialState, action) {
+//      switch (action.type) {
+//           case "GET_EMOJIS"
+//           default:
+//                return state;
+//      }
+// }
