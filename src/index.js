56322348696cf8b8dec44f79c407e61f8d607031
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-22 09:52:53 -0400

    beginning redux store construction

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import { Provider } from 'react-redux'
+// import { createStore } from 'redux'
+
+// const store = createStore()
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+      <App />
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
