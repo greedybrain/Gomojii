5a8916579f85422cb3b59b8f3ab025514f61e016
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-22 12:32:11 -0400

    moving state up from react to redux

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import { Provider } from 'react-redux'
-// import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import manageEmojis from './reducers/manageEmojis'
 
-// const store = createStore()
+const store = createStore(manageEmojis)
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/reducers/manageEmojis.js
+++ src/reducers/manageEmojis.js
@@ -1,14 +1,28 @@
-// const initialState = {
-//      emojis: [],
-//      emojisCategories: [],
-//      emojisFilteredState: [],
-//      emojiSearchResults: []
-// }
+const initialState = {
+     emojis: [],
+     emojisCategories: [],
+     emojisFilteredState: [],
+     emojiSearchResults: []
+}
 
-// export default function manageEmojis(state = initialState, action) {
-//      switch (action.type) {
-//           case "GET_EMOJIS"
-//           default:
-//                return state;
-//      }
-// }
+const BASE_URL = "https://emoji-api.com/emojis?"
+const ACCESS_KEY = "access_key=d03843fe82d5fde7ef6ac67d80fa41b12aae4321"
+
+export default function manageEmojis(state = initialState, action) {
+     switch (action.type) {
+          case "GET_EMOJIS":
+               fetch(BASE_URL + ACCESS_KEY)
+                    .then(res => res.json())
+                    .then(emojiData => {
+                         this.setState({
+                              emojis: emojiData
+                         })
+                    })
+               return {
+                    ...state,
+                    emojis: [...emojis]
+               }
+          default:
+               return state;
+     }
+}
