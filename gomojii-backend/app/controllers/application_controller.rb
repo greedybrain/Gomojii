49b401facb71c62a944c6f7a597ce36d018dcd6d
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-06-25 18:48:27 -0400

    set up application controller

--- gomojii-backend/app/controllers/application_controller.rb
+++ gomojii-backend/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+     skip_before_action :verify_authenticity_token
+     before_action :set_current_user
+
+     def set_current_user 
+          if session[:user_id]
+               @current_user = User.find(session[:user_id])
+          end
+     end
 end
