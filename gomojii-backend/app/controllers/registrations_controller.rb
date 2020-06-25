560893cb346452279ae059557702704ab71c53e7
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-06-25 18:50:21 -0400

    setup registrations controller

---
+++ gomojii-backend/app/controllers/registrations_controller.rb
@@ -0,0 +1,20 @@
+class RegistrationsController < ApplicationController
+     def create
+          user = User.create!(
+               email:params[:email],
+               password: params[:password],
+               password_confirmation: params[:password]
+          )
+          if user
+               session[:user_id] = user.id 
+               render json: {
+                    status: :created,
+                    user: user
+               }
+          else
+               render json: {
+                    status: 500
+               }
+          end
+     end
+end
