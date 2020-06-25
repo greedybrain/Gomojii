e13f4c293cadbe217bd41a5e974a6029137adc5c
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-06-25 15:38:32 -0400

    commit

--- src/config.js
+++
@@ -1,9 +0,0 @@
-export const retrieveFrom = {
-     endpoints: {
-          baseUrl: "https://emoji-api.com/emojis?",
-          categoriesUrl: "https://emoji-api.com/categories?"
-     },
-     security: {
-          accessKey: "access_key=d03843fe82d5fde7ef6ac67d80fa41b12aae4321"
-     }
-}
