9adc1f3d986305028d80e2b6ed1e472c56ca256b
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-26 11:19:08 -0400

    init registrations component

---
+++ gomojii-client/src/auth/Registration.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Registration extends Component {
+
+     handleEmailChange = event => {
+          //todo: updates email field as one types
+          const email = event.target.value
+          this.props.setEmail(email)
+     }
+
+     handlePasswordChange = event => {
+          //todo: updates password field as one types
+          const password = event.target.value
+          this.props.setPassword(password)
+     }
+
+     handleSubmit = event => {
+          const { email, password, setEmail, setPassword, loginUser } = this.props
+          
+          loginUser(email, password)
+
+          //todo: resetting form fields
+          setEmail('')
+          setPassword('')
+          
+          event.preventDefault()
+     }
+     
+     render() {
+          return ( 
+               <form onSubmit={this.handleSubmit}>
+                    <div className="email">
+                         <input
+                              type="email"
+                              name="email"
+                              placeholder="Email"
+                              value={this.props.email}
+                              onChange={this.handleEmailChange}
+                              required
+                         />
+                    </div>
+                    <div className="password">
+                         <input
+                              type="password"
+                              name="password"
+                              placeholder="Password"
+                              value={this.props.password}
+                              onChange={this.handlePasswordChange}
+                              required
+                         />
+                    </div>
+                    <button type="submit">Login</button>
+               </form>
+          );
+     }
+}
+
+const mapStateToProps = state => ({
+     email: state.authRed.email,
+     username: state.authRed.username,
+     password: state.authRed.password,
+})
+
+const mapDispatchToProps = dispatch => ({
+     setEmail: email => dispatch(setUserEmail(email)),
+     setPassword: password => dispatch(setUserPassword(password)),
+     loginUser: (email, password) => dispatch(getExistingUser(email, password))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Registration);
