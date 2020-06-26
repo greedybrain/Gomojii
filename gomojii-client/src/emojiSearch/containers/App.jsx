c04d16cd5915f7aa699afb7ed41a4a2a368b21a5
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-26 12:23:27 -0400

    added handle logout funtionality

--- gomojii-client/src/emojiSearch/containers/App.jsx
+++ gomojii-client/src/emojiSearch/containers/App.jsx
@@ -4,36 +4,51 @@ import EmojiContainer from './EmojiContainer';
 import { connect } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { loadEmojis, loadCategories } from '../../store/middleware/apiEmojiSearch';
-// import { validateSession } from '../../store/middleware/serverAuth';
+import { validateSession, endUsersSession } from '../../store/middleware/serverAuth';
 import Login from '../../auth/Login';
+import Registration from '../../auth/Registration';
 
 class App extends Component {
 
   //todo: inside this lifecycle method I'm loading the emojis and categories to work with later on throughout the application
   componentDidMount() {
     //todo: getting the below methods from props
-    const { loadEmojis, loadCategories, validateSession } = this.props
+    const { user, loadEmojis, loadCategories, validateSession, loggedInStatus } = this.props
     loadEmojis()
     loadCategories()
-    // validateSession()
+    validateSession(loggedInStatus)
+    console.log(user)
+  }
+
+  handleLogout = () => {
+    this.props.logoutUser()
+    console.log(this.props.user)
   }
 
   render() {
     return (
       <div className="App">
         <Login />
+        <Registration />
+        <button onClick={this.handleLogout} type="submit">Logout</button>
         <EmojiContainer />
       </div>
     );
   }
 }
 
+const mapStateToProps = state => ({
+  loggedInStatus: state.authRed.loggedInStatus,
+  user: state.authRed.user
+})
+
 //todo: mapping my dispatch to props 
 const mapDispatchToProps = dispatch => ({
   loadEmojis: () => dispatch(loadEmojis()),
   loadCategories: () => dispatch(loadCategories()),
-  // validateSession: () => dispatch(validateSession())
+  validateSession: status => dispatch(validateSession(status)),
+  logoutUser: () => dispatch(endUsersSession())
 })
 
 //todo: using connect() allows me to 'connect' a component to the store
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
