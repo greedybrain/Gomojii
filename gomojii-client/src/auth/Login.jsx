581b866069c0fbaf979a4d67089dfa4df51399df
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-28 11:27:25 -0400

    changed login component to a functional component

--- gomojii-client/src/auth/Login.jsx
+++ gomojii-client/src/auth/Login.jsx
@@ -1,82 +1,75 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { getExistingUser } from '../store/middleware/serverAuth';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { setUserEmail, setUserPassword } from '../store/manageAuthReducer';
 import { Link } from 'react-router-dom';
 
-class Login extends Component {
+const Login = (props) => {
+     const state = useSelector(state => ({
+          email: state.authRed.email,
+          password: state.authRed.password
+     }))
+     const dispatch = useDispatch()
      
-     handleEmailChange = event => {
+     const handleEmailChange = event => {
           //todo: updates email field as one types
           const email = event.target.value
-          this.props.setEmail(email)
+          dispatch(setUserEmail(email))
      }
 
-     handlePasswordChange = event => {
+     const handlePasswordChange = event => {
           //todo: updates password field as one types
           const password = event.target.value
-          this.props.setPassword(password)
+          dispatch(setUserPassword(password))
      }
 
-     handleSubmit = event => {
-          const { email, password, setEmail, setPassword, loginUser } = this.props
-          loginUser(email, password)
-          //todo: resetting form fields
-          setEmail('')
-          setPassword('')
+     const handleSubmit = event => {
+          const { email, password } = state
+          dispatch(getExistingUser(email, password))
 
-          this.props.history.replace('/')
+          //todo: resetting form fields
+          dispatch(setUserEmail(''))
+          dispatch(setUserPassword(''))
+
+          
+          props.history.replace('/emoji_search')
           
           event.preventDefault()
      }
      
-     render() {
-          return ( 
-               <form className="login-form" onSubmit={this.handleSubmit}>
-                    <div className="logo-on-form">
-                         <img src="images/gomojii-logo.png" alt="logo"/>
-                    </div>
-                    <div className="login-email login-field">
-                         <input
-                              type="email"
-                              name="email"
-                              placeholder="Email"
-                              value={this.props.email}
-                              onChange={this.handleEmailChange}
-                              required
-                         />
-                    </div>
-                    <div className="login-password login-field" >
-                         <input
-                              type="password"
-                              name="password"
-                              placeholder="Password"
-                              value={this.props.password}
-                              onChange={this.handlePasswordChange}
-                              required
-                         />
-                    </div>
-                    <div className="login-btn-cont">
-                         <button className="login-btn" type="submit">Login</button>
-                    </div>
-                    <div className="form-footer">
-                         <span role="img" aria-label="copyright emoji">©️</span> 2020 Gomojii | <Link to="/">Home</Link> 
-                    </div>
-               </form>
-          );
-     }
+     return ( 
+          <form className="login-form" onSubmit={handleSubmit}>
+               <div className="logo-on-form">
+                    <img src="images/gomojii-logo-3.png" alt="logo"/>
+               </div>
+               <div className="login-email login-field">
+                    <input
+                         type="email"
+                         name="email_for_login"
+                         placeholder="Email"
+                         value={state.email}
+                         onChange={handleEmailChange}
+                         required
+                    />
+               </div>
+               <div className="login-password login-field" >
+                    <input
+                         type="password"
+                         name="password_for_login"
+                         placeholder="Password"
+                         value={state.password}
+                         onChange={handlePasswordChange}
+                         required
+                    />
+               </div>
+               <div className="login-btn-cont">
+                    <button className="login-btn" type="submit">Login</button>
+               </div>
+               <div className="form-footer">
+                    <span role="img" aria-label="copyright emoji">©️</span> 2020 Gomojii | <Link to="/emoji_search">Home</Link> 
+               </div>
+          </form>
+     );
 }
 
-const mapStateToProps = state => ({
-     email: state.authRed.email,
-     password: state.authRed.password,
-     user: state.authRed.user
-})
-
-const mapDispatchToProps = dispatch => ({
-     setEmail: email => dispatch(setUserEmail(email)),
-     setPassword: password => dispatch(setUserPassword(password)),
-     loginUser: (email, password) => dispatch(getExistingUser(email, password))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default Login;
