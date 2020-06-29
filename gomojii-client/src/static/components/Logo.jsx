02019972a58d9bf8d3bf0b9e3300c02de50af1d5
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-06-29 16:54:17 -0400

    made logo a home link

--- gomojii-client/src/static/components/Logo.jsx
+++ gomojii-client/src/static/components/Logo.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Logo = () => {
      return (
           <>
-               <img src="images/gomojii-logo-3.png" alt="logo"/>
+               <Link to="/">
+                    <img src="images/gomojii-logo-3.png" alt="logo"/>
+               </Link>
           </>
      );
 }
