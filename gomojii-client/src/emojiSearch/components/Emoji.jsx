accb4f59f4c274b6ce40157150af1063a96d14b8
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-29 10:21:59 -0400

    styling changes

--- gomojii-client/src/emojiSearch/components/Emoji.jsx
+++ gomojii-client/src/emojiSearch/components/Emoji.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-const btnStyles
-
 const Emoji = ({ emoji }) => {
+
+     const copySuccessMsg = () => {
+          alert("COPIED!")
+     }
      return (  
-          <li className="emoji animate__animated animate__fadeIn animate__fast" title={emoji.unicodeName}>
-               {emoji.character}
-               <CopyToClipboard text={emoji.character}>
-                    <button style={{}}>Copy</button>
-               </CopyToClipboard>
-          </li>
+          <CopyToClipboard text={emoji.character}>
+               <li onClick={copySuccessMsg} className="emoji animate__animated animate__fadeIn animate__fast" title={emoji.unicodeName}>
+                    {emoji.character}
+               </li>
+          </CopyToClipboard>
      );
 }
 
