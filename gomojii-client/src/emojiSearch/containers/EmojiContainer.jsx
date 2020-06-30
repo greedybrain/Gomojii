34346c2f0d5761b910b12df3f2627d654165ec2e
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-30 15:33:06 -0400

    removed logout functionality from emojicont comp

--- gomojii-client/src/emojiSearch/containers/EmojiContainer.jsx
+++ gomojii-client/src/emojiSearch/containers/EmojiContainer.jsx
@@ -7,31 +7,22 @@ import { endUsersSession } from '../../store/middleware/serverAuth';
 import Header from '../../static/components/Header';
 
 const EmojiContainer = () => {
-     const dispatch = useDispatch()
-
-     const handleLogout = () => {
-          dispatch(endUsersSession())
-     }
 
      //todo: this is just the container that holds all other parts of the emojiSearch feature
      return (
-          <>
-               <Header handleLogout={handleLogout} />
-               {/* animate__animated animate__bounceInDown */}
-               <div className="wrapper">
-                    <div className="sidebar-nav">
-                         <SidebarFilter />
+          <div className="wrapper">
+               <div className="sidebar-nav">
+                    <SidebarFilter />
+               </div>
+               <div className="form-and-list-cont">
+                    <div className="form">
+                         <EmojiSearchForm />
                     </div>
-                    <div className="form-and-list-cont">
-                         <div className="form">
-                              <EmojiSearchForm />
-                         </div>
-                         <div className="emoji-list">
-                              <EmojiList />
-                         </div>
+                    <div className="emoji-list">
+                         <EmojiList />
                     </div>
                </div>
-          </>
+          </div>
      );
 }
 
