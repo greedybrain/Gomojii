5b4e576b18731b480ce69ddc055bc9b65473edd3
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-30 15:31:10 -0400

    added logout functionality to logout component

--- gomojii-client/src/auth/Logout.jsx
+++ gomojii-client/src/auth/Logout.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { endUsersSession } from '../store/middleware/serverAuth';
 
-const Logout = ({ handleLogout }) => {
+const Logout = () => {
+     const dispatch = useDispatch()
+
+     const logout = () => {
+          dispatch(endUsersSession())
+     }
      return ( 
-          <Link to='#' onClick={handleLogout}>Logout</Link>
+          <Link to='#' onClick={logout}>Logout</Link>
      );
 }
 
