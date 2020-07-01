679bfdce1a5ff732692f8d512c83ce3ff23d3dd9
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-30 21:15:23 -0400

    added user id to emojis table

---
+++ gomojii-backend/db/migrate/20200630204503_add_user_id_to_emojis_table.rb
@@ -0,0 +1,5 @@
+class AddUserIdToEmojisTable < ActiveRecord::Migration[6.0]
+  def change
+    add_column :emojis, :user_id, :integer
+  end
+end
--- gomojii-backend/db/schema.rb
+++ gomojii-backend/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_30_202127) do
+ActiveRecord::Schema.define(version: 2020_06_30_204503) do
 
   create_table "emojis", force: :cascade do |t|
     t.string "slug"
     t.string "character"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
