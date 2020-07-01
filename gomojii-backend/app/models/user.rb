4db4c27d5c2bd3a4d53395178019e483acdc8a86
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-30 21:12:59 -0400

    refactored user model with has many association

--- gomojii-backend/app/models/user.rb
+++ gomojii-backend/app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
      has_secure_password
      validates :username, :email, presence: true, uniqueness: true
 
-     has_many :emojis
+     has_many :emojis, dependent: :destroy
 end
