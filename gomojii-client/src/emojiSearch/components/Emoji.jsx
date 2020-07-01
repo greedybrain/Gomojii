922c9d711d4d414722dcd2b316a7f301dc0c9fc2
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-30 21:16:20 -0400

    added favorites counter method to emojilist

--- gomojii-client/src/emojiSearch/components/Emoji.jsx
+++ gomojii-client/src/emojiSearch/components/Emoji.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { useDispatch, useSelector } from 'react-redux';
+import { saveEmoji } from '../../store/middleware/apiEmojiSearch';
 
 const Emoji = ({ emoji }) => {
      const copySuccessMsg = () => {
           alert("COPIED!")
      }
+     const state = useSelector(state => ({
+          userData: state.authRed.userData
+     }))
+     const { emojis } = state.userData.user.data.attributes
+     const dispatch = useDispatch()
+
+     const handleSaveEmoji = () => {
+          const { slug, character } = emoji
+          dispatch(saveEmoji(slug, character))
+          emojis.length++
+     }
+
      return (  
           <CopyToClipboard text={emoji.character}>
-               <li onDoubleClick={copySuccessMsg} className="emoji animate__animated animate__fadeIn animate__fast" title={emoji.unicodeName || emoji.slug}>
+               <li onClick={handleSaveEmoji} onDoubleClick={copySuccessMsg} className="emoji animate__animated animate__fadeIn animate__fast" title={emoji.unicodeName || emoji.slug}>
                     {emoji.character}
                </li>
           </CopyToClipboard>
