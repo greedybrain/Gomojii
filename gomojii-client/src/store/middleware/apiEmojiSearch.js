beb7ce7d488e99b77b86b1c0567aca0b851de8ac
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-30 21:18:34 -0400

    imported new files

--- gomojii-client/src/store/middleware/apiEmojiSearch.js
+++ gomojii-client/src/store/middleware/apiEmojiSearch.js
@@ -1,27 +1,25 @@
 //! importing necessary files and/or libraries
 import { retrieveFrom } from '../../config'
 import axios from 'axios'
-// import moment from 'moment'
 import {
      startLoadEmojisRequest,
      addEmojis, 
      startLoadCategories, 
-     addCategories
+     addCategories,
+     userSavesEmoji,
+     loadAllUsersSavedEmojis
 } from '../manageEmojisReducer'
 
 //todo: returns my endpoint urls from my config file
 //todo: After retrieving the endpoints and access key, I pick them out using destructuring 
 const { baseEndpoint, categoriesUrl } = retrieveFrom.thirdPartyApiEndpoints
+const { baseUrl, users_path, emojis_path } = retrieveFrom.backendServerEndpoints
 const { accessKey } = retrieveFrom.security
 
 export const loadEmojis = () => {
      //todo: funtion returned in enhanced thunk action creator 
      //todo: Fetching all emojis after a successful load
      return async dispatch => {
-          // const { lastEmojisFetch } = getState().emojisRed
-          // const diffInHours = moment().diff(moment(lastEmojisFetch), 'minutes')
-          // if (diffInHours < 10) return;
-
           dispatch(startLoadEmojisRequest())
           const response = await axios({ url: baseEndpoint + accessKey })
           const emojis = response.data
@@ -38,4 +36,36 @@ export const loadCategories = () => {
           const categories = response.data
           dispatch(addCategories(categories))
      }
+}
+
+export const saveEmoji = (slug, character) => {
+     //todo: funtion returned in enhanced thunk action creator 
+     //todo: Fetch post request to save an emoji
+     return async (dispatch, getState) => {
+          const { data } = getState().authRed.userData.user
+          const response = await axios
+               .post(
+                    `${baseUrl}${users_path}/${data.id}${emojis_path}`,
+                    { slug, character },
+                    { withCredentials: true }
+               )
+          const emoji = response.data.emoji.data
+          dispatch(userSavesEmoji(emoji))
+     }
+}
+
+export const loadUsersSavedEmojis = () => {
+     //todo: funtion returned in enhanced thunk action creator 
+     //todo: Fetch request to get all of a current users saved emojis
+     return async (dispatch, getState) => {
+          const { data } = getState().authRed.userData.user
+          const response = await axios(
+               {
+                    url: `${baseUrl}${users_path}/${data.id}`
+               },
+               { withCredentials: true }
+          )
+          const savedEmojis = response.data.user.data.attributes.emojis
+          dispatch(loadAllUsersSavedEmojis(savedEmojis))
+     }    
 }
