e3797260763dba6cdd32c17420a60ec657535bc3
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-03 20:47:36 -0400

    created headerifloggedin component

---
+++ gomojii-client/src/static/components/HeaderIfLoggedIn.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { helper } from '../../helper'
+import Logo from './Logo'
+import { Link, NavLink } from 'react-router-dom'
+import Logout from '../../auth/Logout'
+
+const HeaderIfLoggedIn = () => {
+     const state = useSelector(state => ({
+          userData: state.authRed.userData,
+     }))
+     const { currentUser, currentUsersEmojis } = helper
+     return (
+          <header className="header-logged-in">
+               <div className="logo-area-header">
+                    <Logo />
+               </div>
+               <ul className="nav-btns">
+                    <li className="home-link">
+                         <NavLink activeClassName="active" to="/emojis">
+                              <span className="home-btn nav-icon" role="img" aria-label="home emojis">🏠</span> Home
+                         </NavLink>
+                    </li>
+                    <li className="favmojis nav-link">
+                         <Link to="#">
+                              <div
+                                   className="loading-indicator-in-header">
+                                   <span className="heart-moji" role="img" aria-label="heart emoji">❤️</span>
+                                   <span className="save-count">{ currentUsersEmojis(state.userData).length }</span>
+                              </div>
+                         </Link>
+                    </li>
+                    <li className="username nav-link">
+                         <i className="fas fa-user user-icon"></i>
+                         { currentUser(state.userData).username }
+                    </li>
+                    <li className="logout-btn nav-link">
+                         <span role="img" aria-label="peace sign emoji">✌</span><Logout />
+                    </li>
+               </ul>
+          </header>
+     )
+}
+
+export default HeaderIfLoggedIn
