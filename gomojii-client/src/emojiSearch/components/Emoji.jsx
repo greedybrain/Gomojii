679a4b0864398340f1706ec8eb3d04cea5cc225e
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-07-05 13:15:43 -0400

    removed state selector

--- gomojii-client/src/emojiSearch/components/Emoji.jsx
+++ gomojii-client/src/emojiSearch/components/Emoji.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard'
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { saveEmoji } from '../../store/middleware/apiEmojiSearch';
 
 const Emoji = ({ emoji }) => {
      const dispatch = useDispatch()
-     const state = useSelector(state => ({
-          userData: state.authRed.userData
-     }))
-     const { emojis } = state.userData.user !== undefined ? state.userData.user.data.attributes : 0
      
      const copySuccessMsg = () => {
           // alert("COPIED!")
