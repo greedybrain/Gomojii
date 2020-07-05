cbb97e5255e678b1d7b55f533afcd37312e31282
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-04 22:11:58 -0400

    added login fail message

--- gomojii-backend/app/controllers/api/v1/sessions_controller.rb
+++ gomojii-backend/app/controllers/api/v1/sessions_controller.rb
@@ -11,6 +11,7 @@ class Api::V1::SessionsController < ApplicationController
                }
           else
                render json: {
+                    message: "Invalid username, and/or password",
                     status: 401
                }
           end
