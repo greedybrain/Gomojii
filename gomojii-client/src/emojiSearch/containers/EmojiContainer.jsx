23f68eec824c57c3034aed576595f315c9d7ece1
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-07-05 13:16:53 -0400

    added success and error component to emoji cont

--- gomojii-client/src/emojiSearch/containers/EmojiContainer.jsx
+++ gomojii-client/src/emojiSearch/containers/EmojiContainer.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import EmojiSearchForm from '../components/EmojiSearchForm';
 import EmojiList from '../components/EmojiList';
 import SidebarFilter from '../components/SidebarFilter';
+import EmojiSaveError from '../../static/components/EmojiSaveError';
+import EmojiSaveSuccess from '../../static/components/EmojiSaveSuccess';
 
 const EmojiContainer = () => {
 
      //todo: this is just the container that holds all other parts of the emojiSearch feature
      return (
           <>
+               <EmojiSaveSuccess />
+               <EmojiSaveError /> 
                <div className="wrapper">
                     <div className="sidebar-nav">
                          <SidebarFilter />
