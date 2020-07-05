1f1538658dba8ea464e2ecfa5c1b168115dd2bbb
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-07-05 13:18:39 -0400

    removed useDispatch hook

--- gomojii-client/src/static/components/HeaderIfLoggedIn.jsx
+++ gomojii-client/src/static/components/HeaderIfLoggedIn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { helper } from '../../helper'
 import Logo from './Logo'
 import { Link, NavLink } from 'react-router-dom'
