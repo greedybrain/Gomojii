6fad3de245df213f886a41f74e7e53d71cae8272
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-07-05 13:17:04 -0400

    created dashsidebarnav component

---
+++ gomojii-client/src/static/components/DashSidebarNav.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { helper } from '../../helper'
+import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+
+const DashSidebarNav = () => {
+     const userData = useSelector(state => state.authRed.userData)
+     const { currentUser, userIsLoggedIn } = helper
+
+     const renderWelcomeMessage = () => {
+          return userIsLoggedIn(userData) ? currentUser(userData).username : null
+     }
+
+     return (
+          <>
+               <h2 className="dash-welcome"><span role="img" aria-label="wave emoji">👋🏽</span> { renderWelcomeMessage() }</h2>
+               <ul>
+                    <li>
+                         <div className="dash-sidenav-icon">
+                              <span role="img" aria-label="glance emoji">🧐</span>
+                         </div>
+                         <div>
+                              <NavLink
+                                   to="#"
+                                   activeClassName="dash-sidebar-item-active">
+                                   At A Glance     
+                              </NavLink>
+                         </div>
+                    </li>
+                    <li>
+                         <div className="dash-sidenav-icon">
+                              <span role="img" aria-label="info emoji">ℹ️</span>
+                         </div>
+                         <div>
+                              <NavLink
+                                   to="#"
+                                   activeClassName="dash-sidebar-item-active">
+                                   About     
+                              </NavLink>
+                         </div>
+                    </li>
+                    <li>
+                         <div className="dash-sidenav-icon">
+                              <span role="img" aria-label="email emoji">📩</span>
+                         </div>
+                         <div>
+                              <NavLink
+                                   to="#"
+                                   activeClassName="dash-sidebar-item-active">
+                                   Contact     
+                              </NavLink>
+                         </div>
+                    </li>
+                    <li>
+                         <div className="dash-sidenav-icon">
+                              <span role="img" aria-label="gear emoji">⚙️</span>
+                         </div>
+                         <div>
+                              <NavLink
+                                   to="#"
+                                   activeClassName="dash-sidebar-item-active">
+                                   Settings     
+                              </NavLink>
+                         </div>
+                    </li>
+               </ul>
+          </>
+     )
+}
+
+export default DashSidebarNav
