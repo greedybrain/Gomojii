a9d9d9870a9042f7aa95c4c21859d107152bfd6d
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-07-05 13:15:56 -0400

    created footer component

---
+++ gomojii-client/src/static/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+     return (
+          <>
+               <span role="img" aria-label="copyright emoji">©️</span> 2020 GoMojii
+          </>
+     )
+}
+
+export default Footer
