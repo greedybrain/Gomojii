84a1718f4480a193028434371465cd0c241a734b
EducationPurposesOnly <educationpurposesonly@Nayas-MBP.localdomain>
2020-07-05 13:17:45 -0400

    create DashWidgetSection component

---
+++ gomojii-client/src/static/components/DashWidgetSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DashWidgetSearchForm from './DashWidgetSearchForm'
+
+const DashWidgetSection = () => {
+     return (
+          <>
+               <h3>Widgets</h3>
+               <div className="widget-search-form">
+                    <DashWidgetSearchForm />
+               </div>
+          </>
+     )
+}
+
+export default DashWidgetSection
