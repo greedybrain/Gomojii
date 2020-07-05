2b35ece68464260608e7532c2dd36fbd7d4b1090
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-04 22:14:40 -0400

    added currentUsersSavedEmojis to detect changes

--- gomojii-client/src/static/components/HeaderIfLoggedIn.jsx
+++ gomojii-client/src/static/components/HeaderIfLoggedIn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { helper } from '../../helper'
 import Logo from './Logo'
 import { Link, NavLink } from 'react-router-dom'
@@ -8,8 +8,11 @@ import Logout from '../../auth/Logout'
 const HeaderIfLoggedIn = () => {
      const state = useSelector(state => ({
           userData: state.authRed.userData,
+          currentUsersSavedEmojis: state.emojisRed.currentUsersSavedEmojis
      }))
-     const { currentUser, currentUsersEmojis } = helper
+     const { currentUser } = helper
+     const { userData, currentUsersSavedEmojis } = state
+
      return (
           <header className="header-logged-in">
                <div className="logo-area-header">
@@ -21,18 +24,23 @@ const HeaderIfLoggedIn = () => {
                               <span className="home-btn nav-icon" role="img" aria-label="home emojis">🏠</span> Home
                          </NavLink>
                     </li>
+                    <li className="dashboard">
+                         <NavLink activeClassName="active" to="/dashboard">
+                              <span className="home-btn nav-icon" role="img" aria-label="home emojis">🎮</span> Dashboard
+                         </NavLink>
+                    </li>
                     <li className="favmojis nav-link">
                          <Link to="#">
                               <div
                                    className="loading-indicator-in-header">
                                    <span className="heart-moji" role="img" aria-label="heart emoji">❤️</span>
-                                   <span className="save-count">{ currentUsersEmojis(state.userData).length }</span>
+                                   <span className="save-count">{ currentUsersSavedEmojis.length }</span>
                               </div>
                          </Link>
                     </li>
                     <li className="username nav-link">
                          <i className="fas fa-user user-icon"></i>
-                         { currentUser(state.userData).username }
+                         { currentUser(userData).username }
                     </li>
                     <li className="logout-btn nav-link">
                          <span role="img" aria-label="peace sign emoji">✌</span><Logout />
