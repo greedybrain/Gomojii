38fa2b8ddb78b9d4528178b82784a85d05ae2d73
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-04 22:14:10 -0400

    added set timeout to loadUsersEmojis method

--- gomojii-client/src/emojiSearch/containers/App.jsx
+++ gomojii-client/src/emojiSearch/containers/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import EmojiContainer from './EmojiContainer';
 import { connect } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom'
-import { loadEmojis, loadCategories } from '../../store/middleware/apiEmojiSearch';
+import { loadEmojis, loadCategories, loadUsersSavedEmojis } from '../../store/middleware/apiEmojiSearch';
 import { validateSession } from '../../store/middleware/serverAuth';
 import Login from '../../auth/Login';
 import Registration from '../../auth/Registration';
@@ -11,16 +11,25 @@ import NotFound from '../../static/components/NotFound';
 import { helper } from '../../helper';
 import HeaderIfLoggedIn from '../../static/components/HeaderIfLoggedIn';
 import HeaderIfLoggedOut from '../../static/components/HeaderIfLoggedOut';
+import Dashboard from '../../user/Dashboard';
 
 class App extends Component {
 
   //todo: inside this lifecycle method I'm loading the emojis and categories to work with later on throughout the application
   componentDidMount() {
     //todo: getting the below methods from props
-    const { loadEmojis, loadCategories, validateSession, loggedInStatus } = this.props
+    const {
+      loadEmojis,
+      loadCategories,
+      validateSession,
+      loggedInStatus,
+      loadUsersEmojis
+    } = this.props
+
     loadEmojis()
     loadCategories()
     validateSession(loggedInStatus)
+    setTimeout(() => loadUsersEmojis(), 1000)
   }
 
   renderCorrectHeader = () => {
@@ -34,32 +43,37 @@ class App extends Component {
     const { userIsLoggedIn } = helper
     return (
       <div className="App">
-          { this.renderCorrectHeader() }
-          <Switch >
-            <Route
-              path='/login'
-              render={(props) => {
-                return userIsLoggedIn(userData) ? <Redirect to="/emojis" /> : <Login {...props} />
-              }} 
-            />
+        { this.renderCorrectHeader() }
+        <Switch>
+          <Route
+            path='/login'
+            render={props => {
+              return userIsLoggedIn(userData) ? <Redirect to="/emojis" /> : <Login {...props} />
+            }} 
+          />
 
-            <Route
-              path='/signup'
-              render={(props) => {
-                return userIsLoggedIn(userData) ? <Redirect to="/emojis" /> : <Registration {...props} /> 
-              }}
-            />
-            
-            <Route
-                path='/emojis'
-                render={(props) => <EmojiContainer {...props} />} 
-            />
+          <Route
+            path='/signup'
+            render={props => {
+              return userIsLoggedIn(userData) ? <Redirect to="/emojis" /> : <Registration {...props} /> 
+            }}
+          />
+        
+          <Route 
+            path="/dashboard"
+            render={props => <Dashboard {...props} />}
+          />
+          
+          <Route
+              path='/emojis'
+              render={props => <EmojiContainer {...props} />} 
+          />
+        
+          <Route path="/not_found" component={NotFound} />
 
-            <Route path="/not_found" component={NotFound} />
+          <Redirect to="/not_found" />
 
-            <Redirect to="/not_found" />
-
-          </Switch>
+        </Switch>
       </div>
     );
   }
@@ -76,6 +90,7 @@ const mapDispatchToProps = dispatch => ({
   loadEmojis: () => dispatch(loadEmojis()),
   loadCategories: () => dispatch(loadCategories()),
   validateSession: status => dispatch(validateSession(status)),
+  loadUsersEmojis: () => dispatch(loadUsersSavedEmojis())
 })
 
 //todo: using connect() allows me to 'connect' a component to the store
