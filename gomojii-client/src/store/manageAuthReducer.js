f30ead45b5944ae8d102e4e223e3dd53ef477618
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-06 15:32:37 -0400

    removed some initState props and switch cases

--- gomojii-client/src/store/manageAuthReducer.js
+++ gomojii-client/src/store/manageAuthReducer.js
@@ -2,9 +2,6 @@
 
 //todo: setting my initial state for user authentication
 const initialState = {
-     email: '',
-     username: '',
-     password: '',
      userData: {},
      loggedInStatus: "NOT_LOGGED_IN",
 }
@@ -12,21 +9,6 @@ const initialState = {
 //todo: this is my reducer handling all types of emojiSearch functions eg. adding emojis and searching through them
 export default function manageAuth(state = initialState, action) {
      switch (action.type) {
-          case SET_USER_EMAIL:
-               return {
-                    ...state,
-                    email: action.payload.email
-               }
-          case SET_USER_USERNAME: 
-               return {
-                    ...state,
-                    username: action.payload.username
-               }
-          case SET_USER_PASSWORD:
-               return {
-                    ...state,
-                    password: action.payload.password
-               }
           case PROCEED_IF_LOGGED_IN:
                return {
                     ...state,
@@ -58,34 +40,11 @@ export default function manageAuth(state = initialState, action) {
 
 //! AUTH - ACTION TYPES
 const PROCEED_IF_LOGGED_IN = "PROCEED_IF_LOGGED_IN"
-const SET_USER_EMAIL = "SET_USER_EMAIL"
-const SET_USER_USERNAME = "SET_USER_USERNAME"
-const SET_USER_PASSWORD = "SET_USER_PASSWORD"
 const SIGNUP_USER = "SIGNUP_USER"
 const LOGIN_USER = "LOGIN_USER"
 const LOGOUT_USER = "LOGOUT_USER"
 
 //! AUTH - ACTION CREATORS
-export const setUserEmail = email => ({
-     type: SET_USER_EMAIL,
-     payload: {
-          email
-     }
-})
-
-export const setUserUsername = username => ({
-     type: SET_USER_USERNAME,
-     payload: {
-          username
-     }
-})
-
-export const setUserPassword = password => ({
-     type: SET_USER_PASSWORD,
-     payload: {
-          password
-     }
-})
 
 //todo: checking if user is already logged in
 export const proceedIfUserLoggedIn = (userData, loggedInStatus) => ({
