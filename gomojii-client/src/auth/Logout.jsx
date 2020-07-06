7d8db985f7b0ae5381b68149d41518db041101d1
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-06 15:28:51 -0400

    removed Link Component import

--- gomojii-client/src/auth/Logout.jsx
+++ gomojii-client/src/auth/Logout.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { endUsersSession } from '../store/middleware/serverAuth';
 
