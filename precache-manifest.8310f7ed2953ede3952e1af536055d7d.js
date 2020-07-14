736f8a2a1cbe112709fbbfd41b03a1a2dd3740ad
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-14 10:22:48 -0400

    Updates

---
+++ precache-manifest.8310f7ed2953ede3952e1af536055d7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eddc6fc6c7c32f87ce5b2996085e1599",
+    "url": "/Gomojii/index.html"
+  },
+  {
+    "revision": "7d7b5bc93fea5dfa7a83",
+    "url": "/Gomojii/static/js/2.9888e3d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Gomojii/static/js/2.9888e3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42a426ca86ad01eb720b",
+    "url": "/Gomojii/static/js/main.133b5b1b.chunk.js"
+  },
+  {
+    "revision": "cce7cbe258ba2af2273c",
+    "url": "/Gomojii/static/js/runtime-main.66517842.js"
+  }
+]);
