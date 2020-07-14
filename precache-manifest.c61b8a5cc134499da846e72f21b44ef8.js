4dea52bee5811b670ebefdc3e3419fa859e8bd1e
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-14 10:47:38 -0400

    Updates

---
+++ precache-manifest.c61b8a5cc134499da846e72f21b44ef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c87a33a171a1d0bb5d9b9c5bd4dca5",
+    "url": "/Gomojii/index.html"
+  },
+  {
+    "revision": "4f315d237052f9a65a99",
+    "url": "/Gomojii/static/js/2.94e0b06e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Gomojii/static/js/2.94e0b06e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690a74d77b934094f77e",
+    "url": "/Gomojii/static/js/main.f550f6c2.chunk.js"
+  },
+  {
+    "revision": "cce7cbe258ba2af2273c",
+    "url": "/Gomojii/static/js/runtime-main.66517842.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Gomojii/precache-manifest.8310f7ed2953ede3952e1af536055d7d.js"
+  "/Gomojii/precache-manifest.c61b8a5cc134499da846e72f21b44ef8.js"
 );
 
 self.addEventListener('message', (event) => {
