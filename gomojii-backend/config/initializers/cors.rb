aa1607c58e0dadb740f23458a9c861322d0a9d1c
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-14 09:12:42 -0400

    added pg to gemfile

--- gomojii-backend/config/initializers/cors.rb
+++ gomojii-backend/config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
      end
 
      allow do 
-          origins '>> PRODUCTION DOMAIN GOES HERE <<'
+          origins 'http://www.gomojii.com'
 
           resource '*',
           headers: 'any',
