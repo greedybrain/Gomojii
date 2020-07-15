590de3c486fef5a7280549e8f42971005ef9ee0c
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-14 22:56:49 -0400

    change

--- gomojii-backend/config/initializers/cors.rb
+++ gomojii-backend/config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
      end
 
      allow do 
-          origins 'https://greedybrain.github.io/Gomojii/#/'
+          origins 'http://greedybrain.github.io/Gomojii/#/'
 
           resource '*',
           headers: 'any',
