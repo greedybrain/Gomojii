fee2ff3746d27eda74809fcfa1dc14fa40adbd57
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-21 11:30:56 -0400

    changes made 2

--- gomojii-backend/app/controllers/api/v1/users_controller.rb
+++ gomojii-backend/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::UsersController < ApplicationController
 
+     def index 
+          users = User.all
+          render json: {
+               users: UserSerializer.new(users).serializable_hash
+          }
+     end
+
      def show 
           user = User.find(params[:id])
           render json: {
