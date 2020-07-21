02ebe4ed695adfec6cc66bad325f4435e65712d6
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-21 11:33:32 -0400

    changes made 3

--- gomojii-backend/config/routes.rb
+++ gomojii-backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do 
-      resources :users, only: [:show. :index] do 
+      resources :users, only: [:show, :index] do 
         resources :emojis, only: [:create, :destroy]
       end
       resources :emojis, only: [:index]
