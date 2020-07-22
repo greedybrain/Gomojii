89b9323971c517d1d3ddaea5a4247112e317ae3e
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-21 21:30:07 -0400

    fchange 1

--- gomojii-backend/config/initializers/cors.rb
+++ gomojii-backend/config/initializers/cors.rb
@@ -1,14 +1,4 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
-     # if Rails.env == "production"
-     # allow do 
-     #      origins 'https://greedybrain.github.io/Gomojii/'
-
-     #      resource '*',
-     #      headers: 'any',
-     #      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-     #      credentials: true
-     # end
-     # else
      allow do 
           origins 'http://localhost:3000'
 
@@ -17,6 +7,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
           credentials: true
      end
-     # end
-
 end
